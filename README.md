5e2721ec8a8e20df9cb3838213e1479e41e540e5
Joni Halabi <her@thatdevgirl.com>
2018-12-31 22:33:41 -0500

    Updating npm includes and rewriting gulp scripts

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ This repository is my personal standard base for any static website.  It can and
 ## Getting Started
 1. Fork or clone this repository.
 2. In the repository directory, run `npm install`.
-3. Run the `gulp` script to watch for Sass and Javascript changes.
+3. Run the `gulp` script to build the CSS and Javascript files.
 
 ## Asset Directories
 
 Directory | Description
 --- | ---
-`/assets/_sass/` | Location of all Sass files. Sass files are divided into `base/`, `layout/`, and `component/` directories. The Gulp script will compile and minify each file.
-`/assets/_scripts/` | Location of all Javascript files. Javascript files are divided into `vendor/` and `custom/` directories. Existing libraries go in the `vendor/` directory.  Your code goes in the `custom/` directory. The Gulp script will compile, minify, and concatenate all files into a single `scripts.min.js` file.  You will need to update `gulpfile.js` as you add additional JS files.
-`/assets/css/` | Location of the compiled CSS files.
-`/assets/js/` | Location of the compiled Javascript file.
+`/assets/source/scss/` | Location of all `.scss` files. SCSS files are divided into `base/`, `layout/`, and `component/` directories. The Gulp script will compile the files, as they are listed in `styles.scss` and minify the compiled result.
+`/assets/source/js/` | Location of all Javascript files. Javascript files are divided into `base/` and `component/` directories. Reusable code goes into the `base/` directory. The Gulp script will compile, minify, and concatenate all files (along with jQuery, from npm) into a single `scripts.min.js` file.  You will need to update `gulpfile.js` as you add additional JS files.
+`/assets/build/css/` | Location of the compiled CSS file, `styles.min.css`.
+`/assets/build/js/` | Location of the compiled Javascript file, `scripts.min.js`.
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
-const gulp = require('gulp'),
-      babelify = require('babelify'),
-      browserify = require('browserify'),
-      clean = require('gulp-clean-css'),
-      concat = require('gulp-concat'),
-      rename = require('gulp-rename'),
-      sass = require('gulp-sass'),
-      source = require('vinyl-source-stream'),
-      streamify = require('gulp-streamify'),
-      uglify = require('gulp-uglify'),
-      watch = require('gulp-watch');
+const gulp = require( 'gulp' ),
+      babel = require( 'gulp-babel' )
+      clean = require( 'gulp-clean-css' ),
+      concat = require( 'gulp-concat' ),
+      rename = require( 'gulp-rename' ),
+      sass = require( 'gulp-sass' ),
+      uglify = require( 'gulp-uglify' ),
+      watch = require( 'gulp-watch' );
 
 // Array of JS files, in order by dependency.
 const jsFiles = [
@@ -18,34 +15,29 @@ const jsFiles = [
 ];
 
 // JS build task.
-gulp.task('js', () => {
-	browserify({
-    	entries: jsFiles,
-    	debug: true
-  	})
-    .transform(babelify.configure({
-      presets: ['env']
-    }))
-    .bundle()
-    .pipe(source('scripts.min.js'))
-    .pipe(streamify(uglify()))
-    .pipe(gulp.dest('assets/build/js'));
-});
+gulp.task( 'js', () => {
+  return gulp.src( jsFiles )
+    .pipe( babel( {
+      presets: ['minify', 'es2015']
+    } ) )
+    .pipe( concat( 'scripts.min.js' ) )
+    .pipe( gulp.dest( 'assets/build/js' ) );
+} );
 
 // CSS build task.
-gulp.task('css', function() {
-	return gulp.src('assets/source/scss/styles.scss')
-		.pipe(sass().on('error', sass.logError))
-    .pipe(clean())
-    .pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('assets/build/css'));
-});
+gulp.task( 'css', () => {
+  return gulp.src( 'assets/source/scss/styles.scss' )
+    .pipe( sass().on( 'error', sass.logError ) )
+    .pipe( clean() )
+    .pipe( rename( {suffix: '.min'} ) )
+    .pipe( gulp.dest( 'assets/build/css' ) );
+} );
 
 // Watcher task.
-gulp.task('watch', function() {
-	gulp.watch('assets/source/scss/**/*.scss', ['css']);
-	gulp.watch('assets/source/js/*.js', ['js']);
-});
+gulp.task( 'watch', () => {
+  gulp.watch( 'assets/source/scss/**/*.scss', ['css'] );
+  gulp.watch( 'assets/source/js/*.js', ['js'] );
+} );
 
 // Default task.
-gulp.task('default', ['watch', 'css', 'js']);
+gulp.task( 'default', gulp.series( 'js', 'css' ) );
